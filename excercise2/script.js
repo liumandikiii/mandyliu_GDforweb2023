e0dd05aea1f2a233d436bf0c22565fd0a9d600a8
liumandikiii <mliu04@risd.edu>
2023-12-12 17:07:55 -0500

    q

---
+++ excercise2/script.js
@@ -0,0 +1,17 @@
+﻿// JavaScript source code
+$(document).ready(function(){ 
+
+
+	$("#darkmode").click(function(){
+		$('body').toggleClass('darkmode');
+		if($("body").hasClass('darkmode')){
+			$(this).text("light mode");
+		}else{
+			$(this).text("Dark mode");
+		}
+
+		
+	});
+
+});
+
